7d141686f3430992a2b0859fd143ac5030164172
ghustBoy <111396828+ghustBoy@users.noreply.github.com>
2022-08-16 20:03:28 -0300

    Create CelsToFahr.py

---
+++ CelsToFahr.py
@@ -0,0 +1,3 @@
+celsius = float(input("Por favor digite a temperatura em Celsius: "))
+fahrenheit = (9 * celsius + 160) / 5
+print(f"A temperatura {celsius} em fahrenheit equivale a {fahrenheit:.1f}.")
